b2f5807c26265a169c5433e147cb110e519f9d74
22437 <zhiyuan.wu@hand-china.com>
2019-08-21 17:23:50 +0800

    [ADD]新增DEMO

--- vue-demo/src/router.js
+++ vue-demo/src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -9,16 +8,13 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: () => import(/* webpackChunkName: "about" */ "./views/Home")
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: "/hand-writing",
+      name: "handWriting",
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "./views/HandWritingBoard")
     }
   ]
 });
---
+++ vue-demo/src/views/Home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <div @click="onClickGoItem">手写板</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  methods: {
+    onClickGoItem() {
+      this.$router.push("/hand-writing");
+    }
+  }
+};
+</script>
