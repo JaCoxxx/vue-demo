a42ffbc9ff0c368f2e8f507b235a1d90d28e4482
22437 <zhiyuan.wu@hand-china.com>
2020-04-13 14:40:25 +0800

    fix：修改项目地址

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-demo/dist/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-demo/dist/" : "/"
 };
